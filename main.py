3a357cc551f3f9e047c6ad1a690cccfb1be280ec
umair-hassan-01 <umair.hassan@mindstormstudios.com>
2024-08-17 19:05:49 +0500

    something

--- main.py
+++ main.py
@@ -121,6 +121,9 @@ def post_email(request:email_template):
     print(request.name)
     return "OK"
 
+@app.get('/all' , response_class=HTMLResponse)
+def get_all(request:Request):
+    return my_templates.TemplateResponse("all.html" , {"request":request})
 @app.get('/test')
 def test(request:Request):
     print("Testing..........")
